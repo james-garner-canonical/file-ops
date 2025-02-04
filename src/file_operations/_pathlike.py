b4128d38f5004534eca2b533c32bf26ec65d2466
James Garner <james.garner@canonical.com>
2025-02-04 15:32:33 +1300

    feat: add full protocol

--- src/file_operations/_pathlike.py
+++ src/file_operations/_pathlike.py
@@ -5,10 +5,14 @@ import pathlib
 import typing
 from typing import Self, Sequence, TypeAlias
 
+from ops import pebble
+
 from . import _chown_utils
+from . import _errors
 
 if typing.TYPE_CHECKING:
     import ops
+    import os
 
 
 DIR_DEFAULT_MODE = 0o777
@@ -32,50 +36,100 @@ class _StrPathLike(typing.Protocol):
 class _PurePathProtocol(typing.Protocol):
     @property
     def parts(self) -> tuple[str, ...]: ...
+
     @property
     def drive(self) -> str: ...
+
     @property
     def root(self) -> str: ...
+
     @property
     def anchor(self) -> str: ...
+
     @property
     def name(self) -> str: ...
+
     @property
     def suffix(self) -> str: ...
+
     @property
     def suffixes(self) -> list[str]: ...
+
     @property
     def stem(self) -> str: ...
+
     def __new__(cls, *args: _StrPath, **kwargs: object) -> Self: ...
+
+    # def __init__(self, *args): ...  # constructor isn't be part of our protocol
+
+    # def __reduce__(self): ...
+    # ops.Container isn't pickleable, so:
+    # a) this shouldn't be part of the protocol
+    # b) at runtime ContainerPath should fail to pickle
+    # we can implement this with a __reduce__ property that raises an AttributeError
+
     def __hash__(self) -> int: ...
-    def __fspath__(self) -> str: ...
-    # it would be nice if we could make it so that it's a
-    # type checking error to call (e.g.) open(...) on a fileops.pathlike.Protocol
-    # though it will work with fileops.pathlike.ContainerPath at runtime
-    # due to inheritance from PurePath
-    # unless we del ContainerPath.__fspath__ ?
-    # but will the type checker know about that?
-    # (no, it won't, also this won't work because it's a method on the superclass)
-    # what if we do def __fspath__(self) -> NoReturn: raise NotImplementedError
-    # (that also makes it fail at runtime, but the type checker again doesn't care)
+
+    # def __fspath__(self) -> str: ...
+    # NOTE: By excluding this from the protocol, we can make it a type checking
+    # error to call (e.g.) open(...) on a fileops.pathlike.Protocol.
+    # Unfortunately due to inheritance from PurePath in the current implementation,
+    # it isn't a type error to call (e.g.) open(...) on a ContainerPath,
+    # and PurePath provides an __fspath__ implementation that results in it
+    # being treated like a regular path by open at runtime.
+    # We can make it fail at runtime by (e.g.)
+    # making a __fspath__ property that raises an AttributeError
+    # but the type checker doesn't care about this it seems.
+    # This is because pathlib.PurePath calls os.PathLike.register(PurePath)
+    # -- that is because of the ABC, the type checker assumes the implementation is ok
+    # It's not clear whether to
+    # a) include or remove this method from the protocol
+    # b) make it a runtime error to try to open a ContainerPath
+    #    -- and use *self.parts for methods returning a ContainerPath
+
     def __lt__(self, other: Self) -> bool: ...
+
     def __le__(self, other: Self) -> bool: ...
+
     def __gt__(self, other: Self) -> bool: ...
+
     def __ge__(self, other: Self) -> bool: ...
+
     def __truediv__(self, key: _StrPath) -> Self: ...
+
     def __rtruediv__(self, key: _StrPath) -> Self: ...
+
     def __bytes__(self) -> bytes: ...
+
     def as_posix(self) -> str: ...
+
     def as_uri(self) -> str: ...
+
     def is_absolute(self) -> bool: ...
+
     def is_reserved(self) -> bool: ...
-    def match(self, path_pattern: str) -> bool: ...
+
+    def match(self, path_pattern: str) -> bool: ...  # signature extended in 3.12+
+
+    # def full_match(self, pattern: str, * case_sensitive: bool = False) -> bool: ...  # 3.13+
+
     def relative_to(self, *other: _StrPath) -> Self: ...
+
+    # def is_relative_to(self, other: _StrPath) -> Self: ...  # 3.9+
+
     def with_name(self, name: str) -> Self: ...
+
     def with_suffix(self, suffix: str) -> Self: ...
+
+    # def with_stem(self, stem: str) -> Self: ...  # 3.9+
+
+    # def with_segments(self, *pathsegments) -> Self: ...  # 3.12+ for new subclassing machinery
+
     def joinpath(self, *other: _StrPath) -> Self: ...
+
     @property
     def parents(self) -> Sequence[Self]: ...
+
     @property
     def parent(self) -> Self: ...
 
@@ -83,19 +137,20 @@ class _PurePathProtocol(typing.Protocol):
 class Protocol(_PurePathProtocol, typing.Protocol):
     # pull
     def read_text(self) -> str: ...
+
     def read_bytes(self) -> bytes: ...
 
     # push
     def write_bytes(
         self,
         data: bytes,
+        # pebble args
         permissions: int | None = None,
         user: str | None = None,
         user_id: int | None = None,
         group: str | None = None,
         group_id: int | None = None,
-    ) -> None:
-        ...
+    ) -> None: ...
 
     def write_text(
         self,
@@ -112,13 +167,13 @@ class Protocol(_PurePathProtocol, typing.Protocol):
         # None -> turn '\n' into os.linesep
         # '' | '\n' -> do nothing
         # '\r' | '\r\r' -> replace '\n' with this option
+        # pebble args
         permissions: int | None = None,
         user: str | None = None,
         user_id: int | None = None,
         group: str | None = None,
         group_id: int | None = None,
-    ) -> None:
-        ...
+    ) -> None: ...
 
     # make_dir
     def mkdir(
@@ -126,21 +181,59 @@ class Protocol(_PurePathProtocol, typing.Protocol):
         mode: int = 0o777,  # TODO: check default value with pebble
         parents: bool = False,
         exist_ok: bool = False,
+        # pebble args
         permissions: int | None = None,
         user: str | None = None,
         user_id: int | None = None,
         group: str | None = None,
         group_id: int | None = None,
-    ):
-        ...
+    ) -> None: ...
 
     # remove
-    def rmdir(self):
-        ...
-    def unlink(self, missing_ok: bool = False):
-        ...
+    def rmdir(self) -> None: ...
 
+    def unlink(self, missing_ok: bool = False) -> None: ...
+
+    # list_files
     def iterdir(self) -> typing.Iterable[Self]: ...
+
+    def glob(self, pattern: str, *, case_sensitive: bool = False) -> typing.Iterable[Self]: ...
+
+    def rglob(self, pattern: str, *, case_sensitive: bool = False) -> typing.Iterable[Self]: ...
+
+    def walk(
+        self,
+        top_down: bool = True,
+        on_error: typing.Callable[[OSError], None] | None = None,
+        follow_symlinks: bool = False,
+    ) -> typing.Iterable[tuple[Self, list[str], list[str]]]: ...
+
+    def lstat(self) -> os.stat_result: ...
+    # NOTE: either no stat or no lstat -- I think this reflects pebble's list_files behaviour?
+
+    def owner(self) -> str: ...
+
+    def group(self) -> str: ...
+
+    def exists(self) -> bool: ...
+
+    def is_dir(self) -> bool: ...
+
+    def is_file(self) -> bool: ...
+
+    def is_mount(self) -> bool: ...
+
+    def is_symlink(self) -> bool: ...
+
+    def is_junction(self) -> bool: ...  # TODO: don't include in Protocol since it's always false in our case?
+
+    def is_block_device(self) -> bool: ...  # TODO: pebble only tells us if it's a device, so maybe we provide is_device instead
+
+    def is_char_device(self) -> bool: ...  # TODO: pebble only tells us if it's a device, so maybe we provide is_device instead
+
+    def is_fifo(self) -> bool: ...
+
+    def is_socket(self) -> bool: ...
 
 
 class ContainerPath(pathlib.PurePath):
@@ -269,7 +362,6 @@ class ContainerPath(pathlib.PurePath):
             return self.container.name.__ge__(other.container.name)
         return super().__ge__(other)
 
-
     ####################
     # Protocol methods #
     ####################
@@ -338,13 +430,82 @@ class ContainerPath(pathlib.PurePath):
         ...
 
     # remove
-    def rmdir(self):
-        ...
-    def unlink(self, missing_ok: bool = False):
-        ...
+    def rmdir(self) -> None:
+        if not self.is_dir():
+            raise NotADirectoryError(f"[Errno 20] Not a directory: '{self}'")
+        try:
+            self.container.remove_path(self, recursive=False)
+        except pebble.Error as error:
+            for error_kind in [_errors.Path.something, _errors.Path.something_else]:
+                if error_kind.matches(error):
+                    raise error_kind.exception_from_error(error)
+            raise
 
+    def unlink(self, missing_ok: bool = False) -> None:
+        if self.is_dir():
+            raise ...
+        try:
+            self.container.remove_path(self, recursive=False)
+        except pebble.Error as error:
+            if _errors.Path.FileNotFound.matches(error):
+                if missing_ok:
+                    return
+                raise FileNotFoundError
+                # or
+                raise _errors.Path.FileNotFound.exception_from_error(error)
+            for error_kind in []:
+                if error_kind.matches(error):
+                    raise error_kind.exception_from_error(error)
+            raise
+
+    # list_files
     def iterdir(self) -> typing.Iterable[Self]:
-        ...
+        def generator() -> typing.Iterator[Self]:
+            if not self.is_dir():
+                raise NotADirectoryError
+            file_infos = self.container.list_files(self)
+            for f in file_infos:
+                yield type(self)(f.path, container=self.container)
+        return generator()
+
+    def glob(self, pattern: str, *, case_sensitive: bool = False) -> typing.Iterable[Self]: ...
+
+    def rglob(self, pattern: str, *, case_sensitive: bool = False) -> typing.Iterable[Self]: ...
+
+    def walk(
+        self,
+        top_down: bool = True,
+        on_error: typing.Callable[[OSError], None] | None = None,
+        follow_symlinks: bool = False,
+    ) -> typing.Iterable[tuple[Self, list[str], list[str]]]: ...
+
+    def lstat(self) -> os.stat_result: ...
+    # NOTE: either no stat or no lstat -- I think this reflects pebble's list_files behaviour?
+
+    def owner(self) -> str: ...
+
+    def group(self) -> str: ...
+
+    def exists(self) -> bool: ...
+
+    def is_dir(self) -> bool: ...
+
+    def is_file(self) -> bool: ...
+
+    def is_mount(self) -> bool: ...
+
+    def is_symlink(self) -> bool: ...
+
+    def is_junction(self) -> bool: ...  # TODO: don't include in Protocol since it's always false in our case?
+
+    def is_block_device(self) -> bool: ...  # TODO: pebble only tells us if it's a device, so maybe we provide is_device instead
+
+    def is_char_device(self) -> bool: ...  # TODO: pebble only tells us if it's a device, so maybe we provide is_device instead
+
+    def is_fifo(self) -> bool: ...
+
+    def is_socket(self) -> bool: ...
+
 
 
 class LocalPath(pathlib.Path):
@@ -401,7 +562,6 @@ class LocalPath(pathlib.Path):
         ...
 
 
-
 def _type_check_1(  # pyright: ignore[reportUnusedFunction]
     _container_path: ContainerPath,
     _path: pathlib.Path,
@@ -438,18 +598,55 @@ def _type_check_1(  # pyright: ignore[reportUnusedFunction]
     # we expect PurePath to be incompatible because it lacks read_text etc
 
 
+_StrOrBytesPath: typing.TypeAlias = 'str | bytes | os.PathLike[str] | os.PathLike[bytes]'
 def _type_check_2(  # pyright: ignore[reportUnusedFunction]
     _pure_path_protocol: _PurePathProtocol,
+    _protocol: Protocol,
     _container_path: ContainerPath,
     _local_path: LocalPath,
     _path: pathlib.Path,
     _pure_path: pathlib.PurePath,
 ) -> None:
-    open(_pure_path_protocol)
+    _p: _StrOrBytesPath
+    _pp: os.PathLike[str]
+    open(_pure_path_protocol)  # pyright: ignore[reportArgumentType]
+    _p = _pure_path_protocol  # pyright: ignore[reportAssignmentType]
+    _pp = _pure_path_protocol  # pyright: ignore[reportAssignmentType]
+    open(_protocol)  # pyright: ignore[reportArgumentType]
+    _p = _protocol  # pyright: ignore[reportAssignmentType]
+    _pp = _protocol  # pyright: ignore[reportAssignmentType]
     open(_container_path)
+    _p = _container_path
+    _pp = _container_path
     # it would be nice to make the above a type checking + runtime error
-    # ContainerPath passes because ContainerPath inherits from PurePath
-    # since this is the case, we have _PurePathProtocol.__fspath__ for now
+    # unfortunately ContainerPath passes type checking somehow because it inherits from PurePath
     open(_local_path)
     open(_path)
     open(_pure_path)
+
+
+def _type_check_3():
+    _pp: os.PathLike[str]
+    class AnnoyingABCInheritance0(os.PathLike): ...
+    f0 = AnnoyingABCInheritance0()
+    class AnnoyingABCInheritance1(os.PathLike):
+        def __fspath__(self, incompatible) -> str: ...
+    f1 = AnnoyingABCInheritance1()
+    class AnnoyingABCInheritance2(os.PathLike):
+        def __fspath__(self) -> None: ...
+    f2 = AnnoyingABCInheritance2()
+    class Good:
+        def __fspath__(self) -> str: ...
+    g = Good()
+    class Bad1(Good):
+        def __fspath__(self, incompatible) -> str: ...
+    b1 = Bad1()
+    class Bad2(Good):
+        def __fspath__(self) -> None: ...
+    b2 = Bad2()
+    _pp = f0  # argh!
+    _pp = f1  # argh!
+    _pp = f2  # argh!
+    _pp = g
+    _pp = b1
+    _pp = b2
